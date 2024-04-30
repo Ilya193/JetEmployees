318d9d4bf1655f8d58f056afed60e00724594c4b
ilyapc <ilya.komendantov@list.ru>
2024-04-30 22:06:16 +0300

    data loading result changed

--- features/home/src/main/java/ru/ikom/home/domain/LoadResult.kt
+++ features/home/src/main/java/ru/ikom/home/domain/LoadResult.kt
@@ -1,3 +1,18 @@
 package ru.ikom.home.domain
 
-data class LoadResult<T>(val data: T, val loading: Boolean = false, val error: ErrorType? = null)
+sealed class LoadResult<out T> {
+
+    data class Success<T>(
+        val data: T
+    ): LoadResult<T>()
+
+    data class Loading<T>(
+        val data: T
+    ): LoadResult<T>()
+
+    data class Error<T>(
+        val data: T,
+        val e: ErrorType
+    ): LoadResult<T>()
+
+}
